53d3c6d46d748cfd92955c79966a8b12f1b6efaa
Andrew Boltachev <andrewboltachev@gmail.com>
2015-03-24 00:41:47 +0300

    sort of sorting

--- src/clj/zarnidict0010/api.clj
+++ src/clj/zarnidict0010/api.clj
@@ -8,14 +8,38 @@
 (defrpc get-state []
   {:random (rand-int 100)})
 
+(defn positions [x coll]
+    (flatten (map-indexed #(if (= %2 x) [%1] []) coll)))
 
+(def m "аӓбвгдеёжзийклмнҥоӧпрстуӱфхцчшщъыӹьэюя")
+
+(defn longer [coll desired-length fill-with] (loop [a coll] (if (= (count a) desired-length) a (recur (conj a fill-with)))))
+
+(defn mari_key_fn [getter]
+  (fn [x]
+  (let [x (getter x)
+        char2key (fn [c]
+                   (let [p (positions c (vec m))] (if-not (empty? p) (first p) -1)))]
+    (mapv char2key (vec x))
+    )
+  )
+  )
+
+;'[:find ?x ?y :where [?e :raw-article/term ?x] [?e :raw-article/content ?y]]
 (defrpc get-articles-state []
-  (let [r
-        (take 100 (d/q
-          ;'[:find ?x ?y :where [?e :raw-article/term ?x] [?e :raw-article/content ?y]]
+  (let [
+        r0 (take 15
+              (d/q
           '[:find ?x :where [?e :raw-article/term ?x]]
-          (d/db db/conn)))
+          (d/db db/conn))
+              )
+        max-len (apply max (map (comp count first) r0))
+        r1 (map #(do [
+                      (longer ((mari_key_fn first) %) max-len 0)
+                      %]) r0)
+        r (mapv last (sort-by first r1))
         ]
     {:articles
-      (mapv #(do {:name %}) (mapv first r))
-    }))
+      (mapv #(do {:name (do %)}) (mapv first r))
+    })
+  )
